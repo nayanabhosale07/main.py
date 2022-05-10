b6b64d21866e2a8a002128863176c74cbfba87e5
nayanabhosale07 <nayana.bhosale07@gmail.com>
2022-05-10 06:54:14 +0200

    tried to solve. But need some improvement

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ import cv2
 img = cv2.imread('resource/siemens-star-05.jpg', 0)
 template = cv2.imread('resource/siemens-star-1.jpg', 0)  # this algorithm is done using gray scale
 
-h, w = template.shape[::]
-print(img)
+h, w = template.shape
+#print(img)
 
 methods = [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
 
-for methods in methods:
- img2 = img.copy()
+for method in methods:
+  img2 = img.copy()
 
-result = cv2.matchTemplate(img2, template, methods)
-# this method performs convolution
+  result = cv2.matchTemplate(img2, template, method)
+  # this method performs convolution
 
-min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+  min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-if methods in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-   location = min_loc
-else:
-   location = max_loc
+  if methods in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+     location = min_loc
+  else:
+     location = max_loc
 
-   bottom_right = (location[0] + w, location[1] + h)
-   cv2.rectangle(img2, location, bottom_right, 255, 5)
-   cv2.imshow('Match', img2)
-   cv2.waitKey(0)
-   cv2.destroyAllWindows()
+     bottom_right = (location[0] + w, location[1] + h)
+     cv2.rectangle(img2, location, bottom_right, 255, 5)
+     #cv2.namedWindow('Match', cv2.WINDOW_NORMAL)
+     cv2.imshow('Match', img2)
+     cv2.waitKey(0)
+   # cv2.destroyAllWindows()
